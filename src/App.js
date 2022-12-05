5d36af492e2d8cb6ecc439c1b5539677ed31df1b
“kitazakita” <kitazaki.webdesign@gmail.com>
2022-12-05 16:11:14 +0900

    1205-16:00

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
+import { useCallback } from 'react';
+import Particles from 'react-particles';
+import { loadFull } from 'tsparticles';
+import particlesOptions from './utils/particles.json'; //https://particles.js.org/
 import './css/reset.css';
 import './css/App.css';
 import back_bg from './imgs/back-bg.jpg';
 import Works from './components/Works.js';
 
 function App() {
+  const particlesInit = useCallback(async (engine) => {
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+  // const particlesLoaded = useCallback(async (container) => {}, []);
+
   return (
     <>
+      <Particles
+        id='tsparticles'
+        init={particlesInit}
+        // loaded={particlesLoaded}
+        options={particlesOptions}
+      />
       <div
-        className="back-bg"
+        className='back-bg'
         style={{ backgroundImage: `url(${back_bg})` }}
       ></div>
-      <h1 className="page-title-name">Kitazaki Takanori</h1>
-      <h2 className="page-title">My Portfolio</h2>
 
-      <section id="github">
-        <h2 className="section-sub-title">GitHub Account</h2>
+      <h1 className='page-title-name'>Kitazaki Takanori</h1>
+      <h2 className='page-title'>My Portfolio</h2>
+
+      <section id='github'>
+        <h2 className='section-sub-title'>GitHub Account</h2>
         <p>@kitazakita</p>
         <p>
-          <a href="https://github.com/kitazakita">
+          <a href='https://github.com/kitazakita'>
             https://github.com/kitazakita
           </a>
         </p>
       </section>
 
-      <section id="skill">
-        <h2 className="section-sub-title">Skill</h2>
-        <p className="skill-description">
+      <section id='skill'>
+        <h2 className='section-sub-title'>Skill</h2>
+        <p className='skill-description'>
           HTML / CSS / Sass / JavaScript / React / Next.js / Vite / BootStrap /
           MUI / Git / illustrator / Photoshop / Figma
         </p>
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,51 +1,66 @@
 import React from 'react';
 import data from './works.json';
+import img01 from '../imgs/works-img01.png';
+import img02 from '../imgs/works-img02.png';
+import img03 from '../imgs/works-img03.png';
 
 const Works = () => {
+  const imgArry = [];
+  imgArry.push(img01);
+  imgArry.push(img02);
+  imgArry.push(img03);
+
   return (
-    <section id="works">
-      <h2 className="section-sub-title">Works</h2>
+    <section id='works'>
+      <h2 className='section-main-title'>Works</h2>
+      <p className='section-sub-title'>全て架空のWEBサイトです。</p>
 
       {data.data.map((v, i) => {
-        console.log(v);
         return (
-          <div key={i}>
-            <ul>
-              <li>
-                <h4>タイトル</h4>
-                <p>{v.title}</p>
-              </li>
-              <li>
-                <h4>サイトURL</h4>
-                <p>{v.url}</p>
-              </li>
-              <li>
-                <h4>JS/WEBフレームワーク</h4>
-                <p>{v.jsFramework}</p>
-              </li>
-              <li>
-                <h4>UIフレームワーク</h4>
-                <p>{v.uiFramework}</p>
-              </li>
-              <li>
-                <h4>マークアップ記述</h4>
-                <p>{v.mark}</p>
-              </li>
-              <li>
-                <h4>スタイリング記述</h4>
-                <p>{v.style}</p>
-              </li>
-              <li>
-                <h4>JSライブラリ</h4>
-                <p>{v.jsLib}</p>
-              </li>
-              <li>
-                <h4>デプロイ先</h4>
-                <p>{v.build}</p>
-              </li>
-            </ul>
+          <div className='works-card' key={i}>
+            <div className='flex-wrapper'>
+              <p className='img'>
+                <img src={imgArry[i]} alt='' />
+              </p>
+              <ul>
+                <li>
+                  <h4>タイトル</h4>
+                  <p>{v.title}</p>
+                </li>
+                <li>
+                  <h4>URL</h4>
+                  <p>
+                    <a href={v.url}>{v.url}</a>
+                  </p>
+                </li>
+                <li>
+                  <h4>JSフレームワーク</h4>
+                  <p>{v.jsFramework}</p>
+                </li>
+                <li>
+                  <h4>UIフレームワーク</h4>
+                  <p>{v.uiFramework}</p>
+                </li>
+                <li>
+                  <h4>JSライブラリ</h4>
+                  <p>{v.jsLib}</p>
+                </li>
+                <li>
+                  <h4>デプロイ先</h4>
+                  <p>{v.build}</p>
+                </li>
+                <li>
+                  <h4>マークアップ</h4>
+                  <p>{v.mark}</p>
+                </li>
+                <li>
+                  <h4>スタイリング</h4>
+                  <p>{v.style}</p>
+                </li>
+              </ul>
+            </div>
 
-            <div>
+            <div className='description'>
               <h4>このサイトの特徴</h4>
               <p>{v.desc}</p>
             </div>
