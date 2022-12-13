4035a70b6d098cebb39ac7f25a8b750a239ba8cf
“kitazakita” <kitazaki.webdesign@gmail.com>
2022-12-13 10:52:08 +0900

    12.13AM開始

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useLayoutEffect, useRef } from 'react';
 import Particles from 'react-particles';
 import { loadFull } from 'tsparticles';
 import particlesOptions from './utils/particles.json'; //https://particles.js.org/
@@ -8,22 +8,92 @@ import back_bg from './imgs/back-bg.jpg';
 import Works from './components/Works.js';
 import Arrow from './components/Arrow.js';
 import { useSpring, animated } from 'react-spring';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  // tsparticles
   const particlesInit = useCallback(async (engine) => {
     await loadFull(engine);
   }, []);
 
+  //react-spring
   const [styles, setStyles] = useSpring(() => {
     return {
       fontSize: '8vw',
     };
   });
 
-  const nameArry = [];
-  // nameArry
-  const name = 'Kitazaki Takanori';
-  nameArry.push(name.split(''));
+  //useRef
+  const githubRef = useRef();
+  const skillRef = useRef();
+
+  //TOP Name用
+  let splitNameArry = [];
+  const myName = 'Kitazaki Takanori';
+  splitNameArry.push(myName.split(''));
+  // console.log(splitNameArry[0]);
+
+  //gsap用
+  useLayoutEffect(() => {
+    // -- ANIMATION CODE HERE --
+
+    splitNameArry[0].forEach((c, i) => {
+      gsap.to(c, 0.6, {
+        // ease: Back.easeOut,
+        delay: i * 0.05,
+        startAt: { y: '-50%', opacity: 0 },
+        y: '0%',
+        opacity: 1,
+      });
+    });
+
+    gsap.fromTo(
+      githubRef.current,
+      {
+        // from
+        x: '-30vw',
+        opacity: 0,
+      },
+      {
+        //To
+        scrollTrigger: {
+          trigger: githubRef.current,
+          start: 'top 80%',
+          markers: true,
+        },
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+      }
+    );
+
+    gsap.fromTo(
+      skillRef.current,
+      {
+        // from
+        x: '30vw',
+        opacity: 0,
+      },
+      {
+        //To
+        scrollTrigger: {
+          trigger: skillRef.current,
+          start: 'top 80%',
+          markers: true,
+        },
+        x: 0,
+        opacity: 1,
+        duration: 1.2,
+      }
+    );
+
+    return () => {
+      // cleanup code (optional)
+    };
+  }, []);
 
   return (
     <>
@@ -37,11 +107,7 @@ function App() {
         style={{ backgroundImage: `url(${back_bg})` }}
       ></div>
 
-      <h1 className='page-title-name'>
-        {nameArry[0].map((v, i) => {
-          return <span key={i}>{v}</span>;
-        })}
-      </h1>
+      <h1 className='page-title-name'>{myName}</h1>
 
       <animated.h2
         style={styles}
@@ -54,7 +120,7 @@ function App() {
 
       <Arrow />
 
-      <section id='github'>
+      <section id='github' ref={githubRef}>
         <h2 className='section-sub-title'>GitHub Account</h2>
         <p>@kitazakita</p>
         <p>
@@ -66,7 +132,7 @@ function App() {
 
       <Arrow />
 
-      <section id='skill'>
+      <section id='skill' ref={skillRef}>
         <h2 className='section-sub-title'>Skill</h2>
         <p className='skill-description'>
           HTML / CSS / Sass / JavaScript / React / Next.js / Vite / BootStrap /
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import 'swiper/css/pagination';
+import { Autoplay, EffectCards, Pagination } from 'swiper';
+
 import works from './works.json';
 import img01 from '../imgs/works-img01.png';
 import img02 from '../imgs/works-img02.png';
@@ -14,58 +20,72 @@ const Works = () => {
       <h2 className='section-main-title section-sub-title'>Works</h2>
       <p className='section-sub-title'>全て架空のWEBサイトです。</p>
 
-      {works.data.map((v, i) => {
-        return (
-          <div className='works-card' key={i}>
-            <div className='flex-wrapper'>
-              <p className='img'>
-                <img src={imgArry[i]} alt='' />
-              </p>
-              <ul>
-                <li>
-                  <h4>タイトル</h4>
-                  <p>{v.title}</p>
-                </li>
-                <li>
-                  <h4>URL</h4>
-                  <p>
-                    <a href={v.url}>{v.url}</a>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, EffectCards]}
+        className='mySwiper'
+        loop={true}
+      >
+        {works.data.map((v, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <div className='works-card'>
+                <div className='flex-wrapper'>
+                  <p className='img'>
+                    <img src={imgArry[i]} alt='' />
                   </p>
-                </li>
-                <li>
-                  <h4>JSフレームワーク</h4>
-                  <p>{v.jsFramework}</p>
-                </li>
-                <li>
-                  <h4>UIフレームワーク</h4>
-                  <p>{v.uiFramework}</p>
-                </li>
-                <li>
-                  <h4>JSライブラリ</h4>
-                  <p>{v.jsLib}</p>
-                </li>
-                <li>
-                  <h4>デプロイ先</h4>
-                  <p>{v.build}</p>
-                </li>
-                <li>
-                  <h4>マークアップ</h4>
-                  <p>{v.mark}</p>
-                </li>
-                <li>
-                  <h4>スタイリング</h4>
-                  <p>{v.style}</p>
-                </li>
-              </ul>
-            </div>
+                  <ul>
+                    <li>
+                      <h4>タイトル</h4>
+                      <p>{v.title}</p>
+                    </li>
+                    <li>
+                      <h4>URL</h4>
+                      <p>
+                        <a href={v.url}>{v.url}</a>
+                      </p>
+                    </li>
+                    <li>
+                      <h4>JSフレームワーク</h4>
+                      <p>{v.jsFramework}</p>
+                    </li>
+                    <li>
+                      <h4>UIフレームワーク</h4>
+                      <p>{v.uiFramework}</p>
+                    </li>
+                    <li>
+                      <h4>JSライブラリ</h4>
+                      <p>{v.jsLib}</p>
+                    </li>
+                    <li>
+                      <h4>デプロイ先</h4>
+                      <p>{v.build}</p>
+                    </li>
+                    <li>
+                      <h4>マークアップ</h4>
+                      <p>{v.mark}</p>
+                    </li>
+                    <li>
+                      <h4>スタイリング</h4>
+                      <p>{v.style}</p>
+                    </li>
+                  </ul>
+                </div>
 
-            <div className='description'>
-              <h4>このサイトの特徴</h4>
-              <p>{v.desc}</p>
-            </div>
-          </div>
-        );
-      })}
+                <div className='description'>
+                  <h4>このサイトの特徴</h4>
+                  <p>{v.desc}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </section>
   );
 };
